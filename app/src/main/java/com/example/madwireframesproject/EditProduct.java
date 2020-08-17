433f7ca3fbfde81efb3c3adc5adda378fe1930a8
Irshad <mohomedirshad759@gmail.com>
2020-08-17 12:10:05 +0530

    IT19009964 Irshad (Products)

---
+++ app/src/main/java/com/example/madwireframesproject/EditProduct.java
@@ -0,0 +1,13 @@
+package com.example.madwireframesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
---
+++ app/src/main/java/com/example/madwireframesproject/MaleProductsViewPageCustomer.java
@@ -0,0 +1,14 @@
+package com.example.madwireframesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaleProductsViewPageCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_male_products_view_page_customer);
+    }
+}
